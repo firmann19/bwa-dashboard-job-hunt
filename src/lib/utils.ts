d0dfd3d35376b88b7af8a60e611386326f1491bd
Firman Ramadhan <firman19ramadhan@gmail.com>
2023-10-15 22:05:14 +0700

    push malam

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,26 +1,26 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-//import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs"
 import moment from "moment";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-{/* export const hashPassword = async (password: string) => {
+ export const hashPassword = async (password: string) => {
 	const hashedPassword = await bcrypt.hash(password, 8);
 
 	return hashedPassword;
-}; */}
+}; 
 
-{/* export const comparePassword = async (
+ export const comparePassword = async (
 	password: string,
 	hashedPassword: string
 ) => {
 	const isMatch = await bcrypt.compare(password, hashedPassword);
 
 	return isMatch;
-}; */}
+};
 
 export async function fetcher<JSON = any>(
 	input: RequestInfo,
