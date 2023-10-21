2dd225e41750d6da8ea25f9193041798cbd27e7f
Firman Ramadhan <firman19ramadhan@gmail.com>
2023-10-21 13:12:56 +0700

    push siang

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,19 +1,36 @@
-//import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-//import OverviewForm from "@/components/forms/OverviewForm";
-//import SocialMediaForm from "@/components/forms/SocialMediaForm";
-//import TeamForm from "@/components/forms/TeamForm";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getServerSession } from "next-auth";
+import React, { FC } from "react";
+import prisma from "../../../../lib/prisma";
 import OverviewForm from "@/components/forms/OverviewForm";
 import SocialMediaForm from "@/components/forms/SocialMediaForm";
 import TeamForm from "@/components/forms/TeamForm";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-//import { getServerSession } from "next-auth";
-import React, { FC } from "react";
-//import prisma from "../../../../lib/prisma";
 
-interface SettingsPageProps {}
+interface SettingsPageProps { }
 
+export const revalidate = 0;
 
-const SettingsPage: FC<SettingsPageProps> = async ({}) => {
+async function getDetailCompany() {
+	const session = await getServerSession(authOptions);
+
+	const company = await prisma.company.findFirst({
+		where: { id: session?.user.id },
+		include: {
+			Companyoverview: true,
+			CompanySocialMedia: true,
+			CompanyTeam: true,
+		},
+	});
+
+	return company;
+}
+
+const SettingsPage: FC<SettingsPageProps> = async ({ }) => {
+	const company = await getDetailCompany();
+
+	console.log(company);
+
 	return (
 		<div>
 			<div className="font-semibold text-3xl mb-5">Settings</div>
@@ -25,13 +42,13 @@ const SettingsPage: FC<SettingsPageProps> = async ({}) => {
 					<TabsTrigger value="teams">Teams</TabsTrigger>
 				</TabsList>
 				<TabsContent value="overview">
-					<OverviewForm />
+					<OverviewForm detail={company?.Companyoverview[0]} />
 				</TabsContent>
 				<TabsContent value="socialLinks">
-					<SocialMediaForm />
+					<SocialMediaForm detail={company?.CompanySocialMedia[0]} />
 				</TabsContent>
 				<TabsContent value="teams">
-					<TeamForm />
+					<TeamForm teams={company?.CompanyTeam} />
 				</TabsContent>
 			</Tabs>
 		</div>
